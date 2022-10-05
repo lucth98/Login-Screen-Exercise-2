d08e67e144d3a8ad4af96175a0870df76afad51c
lucth98 <lucas@thiess.info>
2022-10-05 19:39:39 +0200

    Initial Commit

---
+++ Login Screen Exercise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login Screen Exercise2
+//
+//  Created by Lucas on 05.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
