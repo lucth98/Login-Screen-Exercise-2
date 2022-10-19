100fc15af695030c6ae7f3d2a3a6a4a3b83acd08
lucth98 <lucas@thiess.info>
2022-10-19 17:49:41 +0200

    finished

--- Login Screen Exercise2/ViewController.swift
+++ Login Screen Exercise2/ViewController.swift
@@ -7,13 +7,113 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  ,UITextFieldDelegate {
+    
+    let correctEmail = "a@b.c"
+    let correctpwd = "1234"
+    
+    //ToDo: Setting up the UITextFields
+    
+    @IBOutlet weak var textFieldEmail: UITextField!
+    
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    @IBOutlet weak var indecator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textFieldEmail.autocorrectionType = UITextAutocorrectionType.no
+        textFieldPassword.autocorrectionType = UITextAutocorrectionType.no
+        
+        
+        
+        textFieldEmail.delegate = self
+        textFieldPassword.delegate = self
     }
-
-
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        guard let email = textFieldEmail?.text else{
+            drawAlert("Error ", "Countaint of Textfield email is nil")
+            return
+        }
+        guard email != "" else{
+            drawAlert("Missing", "Email is empty")
+            return
+        }
+        
+        guard let password = textFieldPassword?.text else{
+            drawAlert("Error ", "Countaint of username email is nil")
+            return
+        }
+        
+        disableUiElements()
+        self.enableIndicator()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+          // This code executes after 2 seconds
+          // Check here if the login was successful
+            
+            self.enableUiElements()
+            self.disableIndicator()
+            if( email != self.correctEmail || password != self.correctpwd){
+                self.drawAlert("Wrong creantinals", "Email or password incorrect")
+            }else{
+                self.drawAlert("Locked in", "Locked in")
+                self.cleanElements()
+            }
+        }
+        
+    }
+    
+    func drawAlert(_ titleOfAlert:String,_ messageOfAlert:String){
+        let alert:UIAlertController=UIAlertController(title: titleOfAlert,
+                                                      message: messageOfAlert,
+                                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.destructive))
+    
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func enableIndicator(){
+        indecator.isHidden = false
+    }
+    
+    func disableIndicator(){
+        indecator.isHidden = true
+    }
+    
+    func disableUiElements(){
+        textFieldEmail.isEnabled = false
+        textFieldPassword.isEnabled = false
+        logInButton.isEnabled = false
+    }
+    
+    func enableUiElements(){
+        textFieldEmail.isEnabled = true
+        textFieldPassword.isEnabled = true
+        logInButton.isEnabled = true
+    }
+    
+    func cleanElements(){
+        textFieldEmail.text = ""
+        textFieldPassword.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if(textField == textFieldEmail){
+            textFieldPassword.becomeFirstResponder()
+        }else{
+            loginButtonPressed(textField)
+        }
+        
+        return true
+    }
 }
 
